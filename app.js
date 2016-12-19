988716f5c9d0e9fb6b300d6cedd97f29de796f0a
youzaiyouzai66 <1620030379@qq.com>
2016-12-19 22:52:59 +0800

    基本的base

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+const express = require('express');
+const cheerio = require('cheerio');
+const superagent = require('superagent');
+
+const PORT = 8081;
+
+var app = express();
+app.get('/',function(req, res, next){
+    superagent.get('https://cnodejs.org/')
+        .end(function(err, sres){
+            if(err){
+                return next(err);
+            }
+            var $ = cheerio.load(sres.text);
+            var items = [];
+            $('#topic_list .topic_title').each(function(idx, element){
+                var $element = $(element);
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
+                });
+            });
+
+            res.send(items);
+        })
+});
+app.listen(PORT, function(){
+    console.log('app is listening at '+PORT);
+});
